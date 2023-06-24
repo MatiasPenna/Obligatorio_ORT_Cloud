cf9a82b4868fb58b387bcbecd014803d5ed81ea0
echan92 <edgardo092@hotmail.com>
2023-06-24 17:18:05 -0300

    prueba

--- images.tf
+++ images.tf
@@ -8,23 +8,3 @@ resource "aws_ecr_repository" "obl_ecr" {
   }
 }
 
-## Build de las imagenes y push
-resource "docker_registry_image" "obl" {
-    for_each = toset(var.repository_list)
-    name = "${aws_ecr_repository.obl_ecr.repository_url}:${each.key}"
-
-    build {
-        context = "../src/${each.key}"
-        dockerfile = "Dockerfile"
-    }  
-}
-
-# Build de la imagen de redis que se encuentra en otro directorio el dockerfile
-resource "docker_registry_image" "obl_cartservice" {
-    name = "${aws_ecr_repository.obl_ecr.repository_url}:cartservice"
-
-    build {
-        context = "../src/cartservice/src"
-        dockerfile = "Dockerfile"
-    }  
-}
