b8cabaf9940419ae7ba108e9181ba92e23d0daab
echan92 <edgardo092@hotmail.com>
2023-06-25 18:26:51 -0300

    pruebas para redis

--- output.tf
+++ output.tf
@@ -5,3 +5,7 @@ output "ecr_url" {
 output "cluster_name" {
   value = aws_eks_cluster.eks_obl_01.name
 }
+
+output "elasticache_url" {
+  value = aws_elasticache_cluster.obl_ecache_cluster.cache_nodes.0.address
+}
