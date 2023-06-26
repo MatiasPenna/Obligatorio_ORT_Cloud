d3eb396134253b81722d3d90d138d2028e7c6956
echan92 <edgardo092@hotmail.com>
2023-06-26 16:57:43 -0300

    test

--- modules/build/scripts.tf
+++ modules/build/scripts.tf
@@ -16,15 +16,12 @@
 
 
 resource "null_resource" "script_build" {
-  depends_on = [aws_eks_cluster.eks_obl_01]
-
   provisioner "local-exec" {
     command = "sh ./scripts/build.sh"
   }
 }
 
  resource "null_resource" "script_kube" {
-   depends_on = [null_resource.script_build]
    provisioner "local-exec" {
      command = "sh ./scripts/kube.sh"
    }
