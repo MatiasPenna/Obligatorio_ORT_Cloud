d0e015110df191325becbb3ca24143c2c57f5630
echan92 <edgardo092@hotmail.com>
2023-06-26 19:43:33 -0300

    2

--- modules/Obli/output.tf
+++ modules/Obli/output.tf
@@ -1,11 +1,11 @@
-output "ecr_url" {
+output "ecr_urlmodule" {
   value = aws_ecr_repository.obl_ecr.repository_url
 }
 
-output "cluster_name" {
+output "cluster_namemodule" {
   value = aws_eks_cluster.eks_obl_01.name
 }
 
-output "elasticache_url" {
+output "elasticache_urlmodule" {
   value = aws_elasticache_cluster.obl_ecache_cluster.cache_nodes.0.address
 }
--- output.tf
+++ output.tf
@@ -1,11 +1,11 @@
 output "ecr_url" {
-   value = module.obli.ecr_url
+   value = module.obli.ecr_urlmodule
 }
 
 output "eks_cluster" {
-  value = module.obli.cluster_name
+  value = module.obli.cluster_namemodule
 }
 
 output "ecache_url" {
-  value = module.obli.elasticache_url
+  value = module.obli.elasticache_urlmodule
 }
