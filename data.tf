49ca640b7fec566fb9ea4b95641fe4e06e150c6f
echan92 <edgardo092@hotmail.com>
2023-06-26 16:45:20 -0300

    test

--- data.tf
+++ data.tf
@@ -1,14 +1,14 @@
 data "aws_ecr_repository" "obl_ecr" {
   name = var.namerepoecr
-  depends_on = [aws_ecr_repository.obl_ecr]
+  depends_on = [module.Obli.aws_ecr_repository.obl_ecr]
 }
 
 data "aws_eks_cluster" "eks_cluster" {
   name = var.nameclustereks
-  depends_on = [aws_eks_cluster.eks_obl_01]
+  depends_on = [module.obli.aws_eks_cluster.eks_obl_01]
 }
 
 data "aws_elasticache_cluster" "ecache_cluster" {
   cluster_id = var.clusteridec
-  depends_on = [aws_elasticache_cluster.obl_ecache_cluster]
+  depends_on = [module.obli.aws_elasticache_cluster.obl_ecache_cluster]
 }
