923a483108550be90f8f826c59b4225c0e881def
echan92 <edgardo092@hotmail.com>
2023-06-26 13:54:38 -0300

    acomodo variables

--- modules/Obli/scripts.tf
+++ modules/Obli/scripts.tf
@@ -2,7 +2,7 @@ resource "null_resource" "script_build" {
   depends_on = [aws_eks_cluster.eks_obl_01]
 
   provisioner "local-exec" {
-    command = "sh ./scripts/build.sh ${nameclustereks} ${namerepoecr} ${region}"
+    command = "sh ./scripts/build.sh ${var.nameclustereks} ${var.namerepoecr} ${var.region}"
   }
 }
 
@@ -10,6 +10,6 @@ resource "null_resource" "script_build" {
    depends_on = [null_resource.script_build]
    
    provisioner "local-exec" {
-     command = "sh ./scripts/kube.sh ${obl_ecache} ${namerepoecr}"
+     command = "sh ./scripts/kube.sh ${var.obl_ecache} ${var.namerepoecr}"
    }
  }
