0cca4f2c6546be3070517c2fd0cb6d21b2dc81df
echan92 <edgardo092@hotmail.com>
2023-06-26 16:38:42 -0300

    cambio variables

--- data.tf
+++ data.tf
@@ -1,9 +1,9 @@
 data "aws_ecr_repository" "obl_ecr" {
-  name = var.clusteridec
+  name = var.namerepoecr
 }
 
 data "aws_eks_cluster" "eks_cluster" {
-  name = var.clusteridec
+  name = var.nameclustereks
 }
 
 data "aws_elasticache_cluster" "ecache_cluster" {
