e8f9c1420ec42ffd3d087a9058e07352a2c0f84d
echan92 <edgardo092@hotmail.com>
2023-06-26 20:32:27 -0300

    cambios con data

---
+++ data.tf
@@ -0,0 +1,3 @@
+data "aws_elasticache_cluster" "ecache_cluster" {
+  cluster_id = var.clusteridec
+}
--- output.tf
+++ output.tf
@@ -9,3 +9,7 @@ output "eks_cluster" {
 output "ecache_url" {
   value = module.obli.0.elasticache_urlmodule
 }
+
+output "ecache_url" {
+  value = data.aws_elasticache_cluster.ecache_cluster.cache_nodes[0].address
+}
