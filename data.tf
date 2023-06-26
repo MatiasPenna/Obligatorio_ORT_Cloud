53dc6da4848dc1301642b464dd3eb5f245b8a6a8
echan92 <edgardo092@hotmail.com>
2023-06-26 16:06:45 -0300

    test ecache url

---
+++ data.tf
@@ -0,0 +1,7 @@
+data "aws_elasticache_cluster" "ecache_cluster" {
+  cluster_id = var.clusteridec
+}
+
+output "ecache_url" {
+  value = aws_elasticache_cluster.ecache_cluster.cache_nodes[0].address
+}
