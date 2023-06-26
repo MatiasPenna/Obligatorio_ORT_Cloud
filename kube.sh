7fd139bb4ef884722a1beb2941dd1138cff6920a
echan92 <edgardo092@hotmail.com>
2023-06-25 21:08:10 -0300

    comentamos scripts

--- kube.sh
+++ kube.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
 
+#Cargamos las variables
 ecr_url=$(terraform output -raw ecr_url)
+
+elasticache_url=$(terraform output -raw elasticache_url)
 
 folders="src"
 
-services=$(find "$folders" -maxdepth 1 -type d -printf '%f\n' | grep -v "src")
-
+#Busca todas las rutas a partir de src que contengan un archivo llamado kubernetes-manifests.yaml y hace un bucle
 find "$folders" -type f -name "kubernetes-manifests.yaml" | while read -r file; do
+  #La variable toma el nombre de cada servicio como valor
   srv=$(echo "$file" | awk -F'/' '{print $2}')
-  sed -i 's|<IMAGE:TAG>|'"${ecr_url}:${srv}"'|g' $file
-  kubectl create -f $file
-  sed -i 's|'"${ecr_url}":"${srv}"'|<IMAGE:TAG>|g' $file
+  #Como redis no lo vamos a levantar ejecutamos los comandos si el valor de srv no es redis
+  if [ $srv != "redis" ]; then
+    #buscamos image:tag y lo reemplazamos por la url del ecr ":" y el nombre del servicio
+    sed -i 's|<IMAGE:TAG>|'"${ecr_url}:${srv}"'|g' $file
+    #Si hay algun redis-cart lo reemplazamos por la url del elastic cache
+    sed -i 's|redis-cart|'"${elasticache_url}"'|g' $file
+    #Creamos lo definido dentro de los yaml
+    kubectl create -f $file
+    #Volvemos el valor inmage:tag por defecto
+    sed -i 's|'"${ecr_url}":"${srv}"'|<IMAGE:TAG>|g' $file
+  fi
 done
 
 kubectl get service | grep amazonaws.com | grep -Eo '\S*' | tail -n3 | head -n1
