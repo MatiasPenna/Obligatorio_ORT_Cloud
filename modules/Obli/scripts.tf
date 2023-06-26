c3b9b095211ebf5fba92991285ba688f39dccb6f
echan92 <edgardo092@hotmail.com>
2023-06-26 16:34:44 -0300

    cambios en data y output

--- modules/Obli/scripts.tf
+++ modules/Obli/scripts.tf
@@ -1,10 +1,10 @@
-resource "null_resource" "script_build" {
-  depends_on = [aws_eks_cluster.eks_obl_01]
+# resource "null_resource" "script_build" {
+#   depends_on = [aws_eks_cluster.eks_obl_01]
 
-  provisioner "local-exec" {
-    command = "sh ./scripts/build.sh ${var.nameclustereksmodule} ${var.namerepoecrmodule} ${var.regionmodule}"
-  }
-}
+#   provisioner "local-exec" {
+#     command = "sh ./scripts/build.sh ${var.nameclustereksmodule} ${var.namerepoecrmodule} ${var.regionmodule}"
+#   }
+# }
 
 #  resource "null_resource" "script_kube" {
 #    depends_on = [null_resource.script_build]
@@ -13,3 +13,19 @@ resource "null_resource" "script_build" {
 #      command = "sh ./scripts/kube.sh ${var.clusteridec} ${var.namerepoecrmodule}"
 #    }
 #  }
+
+
+resource "null_resource" "script_build" {
+  depends_on = [aws_eks_cluster.eks_obl_01]
+
+  provisioner "local-exec" {
+    command = "sh ./scripts/build.sh"
+  }
+}
+
+ resource "null_resource" "script_kube" {
+   depends_on = [null_resource.script_build]
+   provisioner "local-exec" {
+     command = "sh ./scripts/kube.sh"
+   }
+ }
--- scripts/kube.sh
+++ scripts/kube.sh
@@ -1,18 +1,11 @@
 #!/bin/bash
 
-ruta_absoluta=$(pwd | sed 's/Obligatorio_ORT_Cloud.*/Obligatorio_ORT_Cloud/')
-
 #Cargamos las variables
+ecr_url=$(terraform output -raw ecr_url)
 
-elasticache_url=$(aws elasticache describe-cache-clusters --cache-cluster-id "$1" --query 'CacheClusters[0].CacheNodes[0].Endpoint.Address' --output text)
-
-ecr_url=$(aws ecr describe-repositories --repository-names "$2" --query 'repositories[0].repositoryUri' --output text)
+ecache_url=$(terraform output -raw ecache_url)
 
 folders="src"
-
-replicas=2
-
-cd "$ruta_absoluta"/deploy
 
 #Busca todas las rutas a partir de src que contengan un archivo llamado kubernetes-manifests.yaml y hace un bucle
 find "$folders" -type f -name "kubernetes-manifests.yaml" | while read -r file; do
@@ -22,12 +15,12 @@ find "$folders" -type f -name "kubernetes-manifests.yaml" | while read -r file; 
   if [ $srv != "redis" ]; then
     line_number=$(awk '/selector:/ {print NR; exit}' $file)
     if [ -n "$line_number" ]; then
-        awk -i inplace -v line=$line_number 'NR==line {print "  replicas: '"${replicas}"'"} {print}' $file
+        awk -i inplace -v line=$line_number 'NR==line {print "  replicas: 2"} {print}' $file
     fi
     #buscamos image:tag y lo reemplazamos por la url del ecr ":" y el nombre del servicio
     sed -i 's|<IMAGE:TAG>|'"${ecr_url}:${srv}"'|g' $file
     #Si hay algun redis-cart lo reemplazamos por la url del elastic cache
-    sed -i 's|redis-cart|'"${elasticache_url}"'|g' $file
+    sed -i 's|redis-cart|'"${ecache_url}"'|g' $file
     #Creamos lo definido dentro de los yaml
     kubectl create -f $file
     #Volvemos el valor inmage:tag por defecto
