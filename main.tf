f0fb610a3124eeb1dd68322b4edaf79af87c5ccf
echan92 <edgardo092@hotmail.com>
2023-06-26 21:23:25 -0300

    2

--- main.tf
+++ main.tf
@@ -44,3 +44,11 @@ nodecachenodesecmodule = var.nodecachenodesec
 namerepoecrmodule = var.namerepoecr
 
 }
+
+data "aws_elasticache_cluster" "redis_cluster" {
+  cluster_id = var.clusteridec
+}
+
+output "ecache_url" {
+  value = data.aws_elasticache_cluster.redis_cluster.cache_nodes[0].address
+}
