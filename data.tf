a0ebbacfda0df54de0934442e45bbeececbea38c
echan92 <edgardo092@hotmail.com>
2023-06-26 21:15:43 -0300

    test

--- data.tf
+++ data.tf
@@ -1,4 +1,4 @@
-data "aws_elasticache_cluster" "ecache_cluster" {
-  cluster_id = var.clusteridec
-  depends_on = [module.obli]
-}
+# data "aws_elasticache_cluster" "ecache_cluster" {
+#   cluster_id = var.clusteridec
+#   depends_on = [module.obli]
+# }
--- output.tf
+++ output.tf
@@ -6,11 +6,11 @@ output "eks_cluster" {
   value = module.obli.0.cluster_namemodule
 }
 
-# output "ecache_url" {
-#   value = module.obli.0.elasticache_urlmodule
-# }
+ output "ecache_url" {
+   value = module.obli.0.elasticache_urlmodule
+ }
 
-output "ecache_url" {
-  #value = data.aws_elasticache_cluster.ecache_cluster.cache_nodes[0].address
-  value = data.aws_elasticache_cluster.ecache_cluster.configuration_endpoint_address
-}
+# output "ecache_url" {
+#   #value = data.aws_elasticache_cluster.ecache_cluster.cache_nodes[0].address
+#   value = data.aws_elasticache_cluster.ecache_cluster.configuration_endpoint_address
+# }
